e0c5c094cab14745dc8b6346426700eb75cdc6eb
Joseph bayad <jobayad66@gmail.com>
2021-08-04 23:39:40 +0100

    with auth hoc

--- client/src/hoc/withAuth.js
+++ client/src/hoc/withAuth.js
@@ -1,5 +1,16 @@
-import useAuth from '../hooks/useAuth';
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { Redirect, Route } from 'react-router-dom';
 
-const WithAuth = (props) => !useAuth(props) && props.children;
+function WithAuth({ children, ...rest }) {
+  const { user } = useSelector((state) => state.auth);
+
+  return (
+    <Route
+      {...rest}
+      render={({ location }) => (user ? children : <Redirect to='/login' />)}
+    />
+  );
+}
 
 export default WithAuth;
