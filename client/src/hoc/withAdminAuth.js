00dcdaf6b751201690d61e2c0ffce2f566ff295a
Joseph bayad <jobayad66@gmail.com>
2021-08-08 18:23:22 +0100

    protect admin rout

--- client/src/hoc/withAdminAuth.js
+++ client/src/hoc/withAdminAuth.js
@@ -1,5 +1,16 @@
-import useAdminAuth from '../hooks/useAdminAuth';
+import React from 'react';
+import { Route } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 
-const WithAdminAuth = (props) => useAdminAuth() && props.children;
+const WithAdminAuth = ({ children, ...rest }) => {
+  const user = useSelector((state) => state.auth.user);
+  const history = useHistory();
+  const isAdmin = user?.isAdmin;
+
+  return (
+    <Route {...rest} render={() => (isAdmin ? children : history.push('/'))} />
+  );
+};
 
 export default WithAdminAuth;
