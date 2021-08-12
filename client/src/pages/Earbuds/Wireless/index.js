a95cdfed55e687f5ac6393c447bd4ef70151d611
Joseph bayad <jobayad66@gmail.com>
2021-08-12 21:53:23 +0100

    wireless earbuds

--- client/src/pages/Earbuds/Wireless/index.js
+++ client/src/pages/Earbuds/Wireless/index.js
@@ -1,6 +1,6 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Links from '../../../components/Links';
-import Products from '../../../features/product/Product';
+import Products from '../../../features/product/Products';
 
 // redux
 import { useDispatch, useSelector } from 'react-redux';
@@ -8,23 +8,22 @@ import { fetchProducts } from '../../../redux/products/productsSlice';
 
 const Wireless = () => {
   const dispatch = useDispatch();
+  const { data, loading } = useSelector((state) => state.products);
+  const wireless = data.filter(
+    (product) =>
+      product.category === 'Earbuds' && product.details.wireless === 'true'
+  );
+
   useEffect(() => {
     if (data?.length > 0) return;
     dispatch(fetchProducts());
   }, []);
-  const { data, loading } = useSelector((state) => state.products);
 
   return (
     <div className='shop'>
       <h1>Wireless Earbuds</h1>
       <Links filter='Earbuds' />
-      <Products
-        loading={loading}
-        data={data.filter(
-          (product) =>
-            product.category === 'Earbuds' && product.wireless === 'true'
-        )}
-      />
+      <Products loading={loading} data={wireless} />
     </div>
   );
 };
