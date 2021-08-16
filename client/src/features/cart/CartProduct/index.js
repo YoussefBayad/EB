e69615e537c9ed75f11f7d7a0dcb85346a731b73
Joseph bayad <jobayad66@gmail.com>
2021-08-16 19:07:36 +0100

    links in cart

--- client/src/features/cart/CartProduct/index.js
+++ client/src/features/cart/CartProduct/index.js
@@ -3,6 +3,7 @@ import { motion } from 'framer-motion';
 import { removeFromCart } from '../../../redux/cart/cartSlice';
 import ProductQty from '../../cart/ProductQty';
 import RemoveProduct from '../RemoveProduct';
+import { Link } from 'react-router-dom';
 import './index.scss';
 
 const CartProduct = ({ product }) => {
@@ -14,17 +15,21 @@ const CartProduct = ({ product }) => {
       exit={{ x: 100, opacity: 0 }}
       transition={{ duration: 0.5 }}
       className='cart-product'>
-      <img
-        className='cart-product-img'
-        src={
-          product.imageURL
-            ? product.imageURL
-            : `/img/${product.name.replace(/\s/g, '')}.webp`
-        }
-        alt={product.name}
-      />
+      <Link to={`/shop/product/${product._id}`}>
+        <img
+          className='cart-product-img'
+          src={
+            product.imageURL
+              ? product.imageURL
+              : `/img/${product.name.replace(/\s/g, '')}.webp`
+          }
+          alt={product.name}
+        />
+      </Link>
       <div className='cart-product-description'>
-        <p className='cart-product-name'>{product.name}</p>
+        <Link to={`/shop/product/${product._id}`}>
+          <p className='cart-product-name'>{product.name}</p>
+        </Link>
         <p className='cart-product-price'>${product.price} usd</p>
         <div className='quantity'>
           <ProductQty
