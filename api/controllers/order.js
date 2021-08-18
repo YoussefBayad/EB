4f9906737953dae57614c8a99fdbc3a6063406c1
Joseph bayad <jobayad66@gmail.com>
2021-08-18 23:27:26 +0100

    get order page style

--- api/controllers/order.js
+++ api/controllers/order.js
@@ -1,5 +1,6 @@
 import Order from '../models/Order.js';
 
+// add order
 export const addOrder = async (req, res) => {
   const data = { ...req.body.order, user: req.user._id };
   console.log('data', data);
@@ -13,12 +14,36 @@ export const addOrder = async (req, res) => {
   }
 };
 
+//get order
+
 export const getOrder = async (req, res) => {
   const order = await Order.findById(req.params.id);
   console.log(order.user == req.user._id);
 
   if (order) {
     res.status(200).json(order);
+  } else {
+    res.status(404).json('There is no order by that id');
+  }
+};
+
+// update order to paid
+
+export const updateOrder = async (req, res) => {
+  const order = await Order.findById(req.params.id);
+
+  if (order) {
+    order.isPaid = true;
+    order.paidAt = date.now();
+    order.paymentResult = {
+      id: req.body.id,
+      status: req.body.status,
+      update_time: req.body.update_time,
+      email_address: req.body.payer.email_address,
+    };
+    const updatesOrder = await Order.save();
+
+    res.status(200).json(updatesOrder);
   } else {
     res.status(404).json('There is no order by that id');
   }
--- api/routes/order.js
+++ api/routes/order.js
@@ -1,10 +1,11 @@
 import express from 'express';
 
 const router = express.Router();
-import { addOrder, getOrder } from '../controllers/order.js';
+import { addOrder, getOrder, updateOrder } from '../controllers/order.js';
 import { userAuth } from '../middleware/auth.js';
 
 router.post('/', userAuth, addOrder);
 router.get('/:id', userAuth, getOrder);
+router.put('/:id/pay', userAuth, updateOrder);
 
 export default router;
