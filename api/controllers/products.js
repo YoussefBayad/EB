a49c9fd2b16442a8aa5b24df81aed1ff9a3ddbbb
Joseph bayad <jobayad66@gmail.com>
2021-08-27 15:03:43 +0100

    add review controller

--- api/controllers/products.js
+++ api/controllers/products.js
@@ -80,6 +80,44 @@ export const deleteProduct = async (req, res) => {
     return res.status(500).json(err);
   }
 };
+
+// create a review
+export const createProductReview = async (req, res) => {
+  const { rating, comment } = req.body;
+
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    const alreadyReviewed = product.reviews.find(
+      (r) => r.user.toString() === req.user._id.toString()
+    );
+
+    if (alreadyReviewed) {
+      res.status(400).error('Product already reviewed');
+    }
+
+    const review = {
+      name: req.user.name,
+      rating: Number(rating),
+      comment,
+      user: req.user._id,
+    };
+
+    product.reviews.push(review);
+
+    product.numReviews = product.reviews.length;
+
+    product.rating =
+      product.reviews.reduce((acc, item) => item.rating + acc, 0) /
+      product.reviews.length;
+
+    await product.save();
+    res.status(201).json({ message: 'Review added' });
+  } else {
+    res.status(404).error('Product not found');
+  }
+};
+
 // const unusedCode = () => {
 // if (!sku) {
 //     return res.status(400).json({ error: 'You must enter sku.' });
