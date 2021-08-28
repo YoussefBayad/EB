61cc0cd880a57881fd0d9fd5005c28409ccdfe6a
Joseph bayad <jobayad66@gmail.com>
2021-08-28 14:20:32 +0100

    review style

--- client/src/features/product/AddReview/index.js
+++ client/src/features/product/AddReview/index.js
@@ -10,9 +10,17 @@ import Rating from '../../../components/Rating';
 
 const AddReview = ({ id, reviews }) => {
   const dispatch = useDispatch();
-  const { username } = useSelector((state) => state.auth.user);
+  const user = useSelector((state) => state.auth.user);
+  const username = user?.username;
+  const userId = user?._id;
+
+  //check if already reviewed
+  const alreadyReviewed = reviews.find(
+    (r) => r.user.toString() === userId.toString()
+  );
 
   // add review
+
   const initialValues = {
     comment: '',
     rating: 0,
@@ -29,58 +37,66 @@ const AddReview = ({ id, reviews }) => {
   return (
     <div className='add-review'>
       <div>
-        <h1>Reviews</h1>
+        <h1>Reviews :</h1>
         {reviews.length === 0 && <Message>No Reviews</Message>}
-        <div variant='flush'>
+        <div>
           {reviews.map((review) => (
-            <div key={review._id}>
-              <strong>{review.name}</strong>
+            <div className='review' key={review._id}>
+              <strong>{review.username}</strong>
               <Rating rating={review.rating} />
-              <p>{review.createdAt.substring(0, 10)}</p>
-              <p>{review.comment}</p>
+              <h6>{review.createdAt.substring(0, 10)}</h6>
+              <p className='comment'>{review.comment}</p>
             </div>
           ))}
           <div>
-            <h3>Write a Customer Review</h3>
+            <h3>Write a Customer Review :</h3>
 
             {username ? (
-              <Formik initialValues={initialValues} onSubmit={addReviewHandler}>
-                <Form>
-                  <div>
-                    <h4>Rating</h4>
-                    <Field
-                      name='rating'
-                      as='select'
-                      // value={rating}
-                      // onChange={(e) => setRating(e.target.value)}
-                    >
-                      <option value=''>Select...</option>
-                      <option value='1'>1 - Poor</option>
-                      <option value='2'>2 - Fair</option>
-                      <option value='3'>3 - Good</option>
-                      <option value='4'>4 - Very Good</option>
-                      <option value='5'>5 - Excellent</option>
-                    </Field>
-                  </div>
-                  <div controlId='comment'>
-                    <h4>Comment</h4>
-                    <Field
-                      as='textarea'
-                      name='comment'
-                      placeholder='write review'
-                    />
-                  </div>
-                  <Button
-                    // disabled={loadingProductReview}
-                    type='submit'>
-                    Add Review
-                  </Button>
-                </Form>
-              </Formik>
+              !alreadyReviewed ? (
+                <Formik
+                  initialValues={initialValues}
+                  onSubmit={addReviewHandler}>
+                  <Form>
+                    <div>
+                      <h4>Rating</h4>
+                      <Field
+                        name='rating'
+                        as='select'
+                        // value={rating}
+                        // onChange={(e) => setRating(e.target.value)}
+                      >
+                        <option value=''>Select...</option>
+                        <option value='1'>1 - Poor</option>
+                        <option value='2'>2 - Fair</option>
+                        <option value='3'>3 - Good</option>
+                        <option value='4'>4 - Very Good</option>
+                        <option value='5'>5 - Excellent</option>
+                      </Field>
+                    </div>
+                    <div controlId='comment'>
+                      <h4>Comment</h4>
+                      <Field
+                        as='textarea'
+                        name='comment'
+                        placeholder='write review'
+                      />
+                    </div>
+                    <Button
+                      // disabled={loadingProductReview}
+                      type='submit'>
+                      Add Review
+                    </Button>
+                  </Form>
+                </Formik>
+              ) : (
+                <p className='login-message'>
+                  You Already reviewed this product
+                </p>
+              )
             ) : (
-              <Message>
+              <p className='login-message'>
                 Please <Link to='/login'>sign in</Link> to write a review{' '}
-              </Message>
+              </p>
             )}
           </div>
         </div>
