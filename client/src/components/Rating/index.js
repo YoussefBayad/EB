6e602c136c272cca71bbd59b714e3fca800c29ed
Joseph bayad <jobayad66@gmail.com>
2021-08-28 14:20:51 +0100

    prevent user from double review on front end

--- client/src/components/Rating/index.js
+++ client/src/components/Rating/index.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import './index.scss';
 const Rating = ({ rating, numReviews }) => {
+  const ratingNumber = Math.ceil(rating);
   return (
     <div className='rating'>
-      {[...Array(rating).keys()].map((s) => (
+      {[...Array(ratingNumber).keys()].map((s) => (
         <span style={{ color: '#ff9933', cursor: 'pointer' }}>{'\u2605'}</span>
       ))}
-      {[...Array(5 - rating).keys()].map((s) => (
+      {[...Array(5 - ratingNumber).keys()].map((s) => (
         <span style={{ color: '#ff9933', cursor: 'pointer' }}>{'\u2606'}</span>
       ))}
 
-      <p>{`${numReviews}`} reviews</p>
+      {numReviews && <p>{`${numReviews}`} reviews</p>}
     </div>
   );
 };
