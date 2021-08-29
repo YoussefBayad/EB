e876bd395ea9ed382259ea318d345080513545e4
Joseph bayad <jobayad66@gmail.com>
2021-08-29 15:45:17 +0100

    use front end for files in production mode

--- api/index.js
+++ api/index.js
@@ -24,8 +24,6 @@ app.use(express.json());
 app.use(helmet());
 app.use(morgan('common'));
 
-const __dirname = path.resolve();
-app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 // routes
 app.use('/api/auth', authRoute);
 app.use('/api/products', productsRoute);
@@ -36,6 +34,20 @@ app.get('/api/config/paypal', (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
 );
 
+const __dirname = path.resolve();
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  );
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running....');
+  });
+}
 // Error Handler Middleware
 app.use(errorHandler);
 
