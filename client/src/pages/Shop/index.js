6ac9aaed7d03ff96060e815617b605e3859f665a
Joseph bayad <jobayad66@gmail.com>
2021-08-30 15:28:46 +0100

    show a message and link when o product foud on search

--- client/src/pages/Shop/index.js
+++ client/src/pages/Shop/index.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 
 import { useDispatch, useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import Links from '../../components/Links';
 import Products from '../../features/product/Products';
 import { fetchProducts } from '../../redux/products/productsSlice';
@@ -23,6 +23,11 @@ const Shop = () => {
       <h1>Shop</h1>
       <Links filter='Shop' />
       <Products data={data} loading={loading} message={message} />
+      {data.length === 0 && (
+        <h3 className='no-product-exist'>
+          No product found with this name <Link to='/shop'>Back to Shop</Link>
+        </h3>
+      )}
     </div>
   );
 };
